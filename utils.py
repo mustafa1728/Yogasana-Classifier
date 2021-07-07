cafb0012499ef8130db7dd6b8c244bf0e4a12d2a
Nirjhar Das <69003365+nirjhar-das@users.noreply.github.com>
2021-07-07 15:41:48 +0530

    Util package
    ArgParse added for argument parsing from command line

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import argparse as args
 
 def pre_process_labels(dataset):
     class_to_id_mapping = {} 
@@ -20,3 +21,8 @@ def create_train_test(X, Y, test_size):
     sc_X = StandardScaler()
     X_Train = sc_X.fit_transform(X_Train)
     X_Test = sc_X.transform(X_Test)
+
+def get_parent_args():
+    parent_args = args.ArgumentParser(add_help=False)
+    parent_args.add_argument('--model_path', '-m', dest='model_path', nargs=1, help='Specify the Trained Model Path')
+    parent_args.add_argument()
