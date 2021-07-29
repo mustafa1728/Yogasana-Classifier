c7685b8a5bfd19e65b59204248da781753bda031
Mustafa Chasmai <mustchasmai@gmail.com>
2021-07-29 19:11:22 +0530

    store predictions and labels in csv

--- classifier/test_folds.py
+++ classifier/test_folds.py
@@ -4,7 +4,7 @@ from numpy import genfromtxt
 from utils import merge_dicts
 from model import Classifier
 
-def evaluate_fold(folder_path, X, Y, confusion_path= "confusion_fold.png", method = "random_forest", max_depth = 8, no_trees = 500, lr = 5):
+def evaluate_fold(folder_path, X, Y, confusion_path= "confusion_fold.png", method = "random_forest", max_depth = 8, no_trees = 500, lr = 5, predictions = None):
     train_idx = genfromtxt(os.path.join(folder_path, 'train_idx.txt'), delimiter='\n')
     #Y_train = genfromtxt(os.path.join(folder_path, 'y_train.csv'), delimiter=',')
     test_idx = genfromtxt(os.path.join(folder_path, 'test_idx.txt'), delimiter='\n')
@@ -20,7 +20,9 @@ def evaluate_fold(folder_path, X, Y, confusion_path= "confusion_fold.png", metho
     metric_dict = classifier.evaluate(X_test, Y_test, confusion_path = confusion_path)
     #print("The {} classifier with {} decision trees has an accuracy of {}%".format(method, no_trees, 100*accuracy))
 
-    return metric_dict
+    predictions[test_idx] = classifier.model.predict(X_test)
+
+    return metric_dict, predictions
     
 def main():
     root_path = "../preprocess/subject_wise_fold/"
@@ -34,16 +36,21 @@ def main():
     X = genfromtxt(os.join.path(data_path, 'X_sub_sampled.csv'), delimiter=',')
     Y = genfromtxt(os.join.path(data_path, 'Y_sub_sampled.csv'), delimiter=',')
 
+    predictions = [-1 for i in range(len(Y))]
     md = {}
 
     for i in range(len(folders_list)):
-        metrtic_dict = evaluate_fold(folders_list[i], X, Y, os.path.join(confusion_root, "fold_" + str(i) + ".png"))
+        metrtic_dict, predictions = evaluate_fold(folders_list[i], X, Y, os.path.join(confusion_root, "fold_" + str(i) + ".png", predictions=predictions))
         md = merge_dicts(md, metrtic_dict, i)
     
     df = pd.DataFrame(md)
     df['Metrics'] = df.index
     df = df[[df.columns.tolist()[-1]] + df.columns.tolist()[:-1]]
     df.to_csv("subject_wise_results_10_fold.csv", index = False)
+
+    df_predictions = pd.DataFrame({"labels": Y, "predictions": predictions})
+    df_predictions.to_csv("predictions_subj_wise.csv")
+
         
 if __name__ == "__main__":
     main()
