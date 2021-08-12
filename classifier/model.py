6316bc7f0befc54c34460140227dabf588d85be5
Mustafa Chasmai <mustchasmai@gmail.com>
2021-08-12 19:47:02 +0530

    add VotingClassifier for ensembling

--- classifier/model.py
+++ classifier/model.py
@@ -2,24 +2,26 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.experimental import enable_hist_gradient_boosting
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier, HistGradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier, HistGradientBoostingClassifier, VotingClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
-# from lightgbm import LGBMClassifier
+from lightgbm import LGBMClassifier
 import joblib
 import pickle
 
-from utils import get_dataset, save_confusion
+from utils import save_confusion
 
 class Classifier():
     '''
-        Generic classifier 
+        Generic classifier providing unified API for different classifiers
         Currently supported methods:
             adaboost
             random_forest
             bagging
             grad_boost
             hist_grad_boost
+            lgbm
+            ensemble (a voting classifier of random_forest, hist_grad_boost and lgbm)
 
         Parameters: 
             max_depth       : maximum depth of any tree
@@ -40,15 +42,21 @@ class Classifier():
         if self.method == "adaboost":
             self.model = AdaBoostClassifier(DecisionTreeClassifier(criterion='gini', max_depth=self.max_depth), n_estimators = self.no_estimators, random_state = self.random_state, learning_rate=self.lr)
         elif self.method == "random_forest":
-            self.model = RandomForestClassifier(n_estimators = self.no_estimators, criterion = 'entropy', random_state = self.random_state, max_depth=self.max_depth)
+            self.model = RandomForestClassifier(n_estimators = self.no_estimators, criterion = 'gini', random_state = self.random_state, max_depth=self.max_depth)
         elif self.method == "bagging":
             self.model = BaggingClassifier(DecisionTreeClassifier(criterion='gini', max_depth=self.max_depth), n_estimators=self.no_estimators, random_state=self.random_state)
         elif self.method == "grad_boost":
             self.model = GradientBoostingClassifier(n_estimators = self.no_estimators, criterion = 'friedman_mse', random_state = self.random_state, max_depth=self.max_depth, learning_rate=self.lr)
         elif self.method == "hist_grad_boost":
             self.model = HistGradientBoostingClassifier(max_iter = self.no_estimators, random_state = self.random_state, max_depth=self.max_depth, learning_rate=self.lr)
-        elif self.method == "LGBM":
-            self.model = LGBMClassifier(boosting_type='goss', max_depth=self.max_depth, n_estimators = self.no_estimators, random_state = self.random_state, learning_rate=self.lr)
+        elif self.method == "lgbm":
+             self.model = LGBMClassifier(boosting_type='goss', max_depth=None, n_estimators = self.no_estimators, random_state = self.random_state, learning_rate=self.lr)
+        elif self.method == "ensemble":
+            clf1 = RandomForestClassifier(n_estimators = self.no_estimators, criterion = 'gini', random_state = self.random_state, max_depth=self.max_depth)
+            clf2 = HistGradientBoostingClassifier(max_iter = self.no_estimators, random_state = self.random_state, max_depth=self.max_depth, learning_rate=0.1)
+            clf3 = LGBMClassifier(boosting_type='goss', max_depth=None, n_estimators = self.no_estimators, random_state = self.random_state, learning_rate=0.1)
+            estimators=[('rf', clf1), ('lgbm', clf2), ('hgrad', clf3)]
+            self.model = VotingClassifier(estimators, voting='soft', weights=[1.5,2,3])
         self.model.fit(X_train,Y_train)
         return self.model
 
@@ -64,7 +72,10 @@ class Classifier():
             model = self.model
         if confusion_path is not None:
             save_confusion(model, X_test, Y_test, save_path = confusion_path)
-        return model.score(X_test, Y_test)
+        if self.method == "LGBM":
+            return accuracy_score(Y_test, self.model.predict(X_test))
+        else:
+            return model.score(X_test, Y_test)
     
     def test(self, X_test, Y_test, model_path, confusion_path = None):
         self.load(model_path)
@@ -78,19 +89,3 @@ class Classifier():
             with open(scaler_save, "wb") as f:
                 pickle.dump(self.sc_X, f)
         return X_train, X_test
-
-def main(no_trees = 200, max_depth = 8, dataset_path = "dataset.csv", save_model_path = "model.z", model = "adaboost"):
-
-    X, Y, _ = get_dataset(dataset_path)
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0, stratify=Y)
-
-    classifier = Classifier(model, max_depth = max_depth, no_estimators = no_trees)
-
-    X_train, X_test = classifier.scale_vectors(X_train, X_test, "scaler.pkl")
-    classifier.train(X_train, Y_train)
-    classifier.save_model(save_model_path)
-    accuracy = classifier.evaluate(X_test, Y_test, confusion_path = "confusion_matrix.png")
-    print("The {} classifier with {} decision trees has an accuracy of {}%".format(model, no_trees, 100*accuracy))
-
-if __name__ == "__main__":
-    main()
