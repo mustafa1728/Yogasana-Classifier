ac912ecaf009e2866884c1c4e7b004f9f12bc5d0
Mustafa Chasmai <mustchasmai@gmail.com>
2021-08-13 17:48:41 +0530

    add scripts for experimenting on yadav dataset

---
+++ datasets/yadav/yadav_pre_process.py
@@ -0,0 +1,120 @@
+import pandas as pd 
+import json
+import os
+
+
+
+main_csv_path = "/Users/mustafa/Desktop/yoga/datasets/yadav et al/yadav_dataset/extracted.csv"
+json_root_path = "/Users/mustafa/Desktop/yoga/datasets/yadav et al/yadav_dataset_kps/"
+
+main_df = pd.read_csv(main_csv_path)
+print(main_df.head())
+print(main_df.columns)
+
+dataset_dict = {"image_path":[], "asana": [], "subject": [], "class": [], "frame":[], "x0": [], "y0": [], "width": [], "height": []}
+for i in range(136):
+	for j in range(3):
+		dataset_dict["keypt" + "_" + str(i) + "_" + str(j)] = []
+
+
+def fix_subjects(subject):
+    if subject == "Sathak":
+        return "Sarthak"
+    if subject == "Shiv":
+        return "Shiva"
+    if subject == "veena":
+        return "Veena"
+    if subject == "deepa":
+        return "Deepa"
+    if subject == "lakshmi":
+        return "Lakshmi"
+    return subject
+    
+
+def fix_class_naming(asana):
+    if asana == "Trik" or asana == "trikon" or asana == "Trikon" or asana == "Trikonasana":
+        return "Trikonasana"
+    if asana == "Bhuj" or asana == "bhuj" or asana == "bhujan" or asana == "Bhuj2" or asana == "bhujang" or asana == "Bhu": 
+        return "Bhujangasana"
+    if asana == "Shav" or asana == "shav" or asana == "shava" or asana == "Shavasana" or asana == "savasan": 
+        return "Shavasana"
+    if asana == "padam" or asana == "Padam" or asana == "padmasan": 
+        return "Padamasana"
+    if asana == "tadasan" or asana == "tadasna" or asana == "Tad" or asana == "Tadasan" or asana == "Tadasna" or asana == "Tadasana" or asana == "Tada": 
+        return "Tadasana"
+    if asana == "vriksh" or asana == "Vriksh" : 
+        return "Vrikshasana"
+    raise ValueError("unhandled asana {}".format(asana))
+
+def update_dict(path, asana, subject, x0, y0, w, h, kps, frame_no):
+    dataset_dict["image_path"].append(path)
+    dataset_dict["asana"].append(asana)
+    dataset_dict["class"].append(fix_class_naming(asana))
+    dataset_dict["subject"].append(fix_subjects(subject))
+    dataset_dict["x0"].append(x0)
+    dataset_dict["y0"].append(y0)
+    dataset_dict["width"].append(w)
+    dataset_dict["height"].append(h)
+    dataset_dict["frame"].append(frame_no)
+    print(kps[0])
+    for i in range(136):
+        for j in range(3):
+            dataset_dict["keypt" + "_" + str(i) + "_" + str(j)].append(kps[i*3 + j])
+
+curated_removed = 0
+total_data = 0
+
+for i, (asana, subject, frame_no, path) in enumerate(zip(main_df["asana"], main_df["subject"], main_df["frame_no"], main_df["path"])):
+
+    print("processing {} - {} - {}".format(asana, subject, frame_no))
+    folder_name = path.split('/')[-2]
+    img_name = path.split('/')[-1]
+
+    curated_root_dir = "/Users/mustafa/Desktop/yoga/datasets/yadav et al/curation/yadav_dataset"
+    # if not os.path.isfile(os.path.join(curated_root_dir, folder_name, img_name)):
+    #     print("removed in curation")
+    #     curated_removed+=1
+    #     continue
+
+    try:
+        with open(os.path.join(json_root_path, folder_name, "alphapose-results.json")) as f:
+            data = json.load(f)
+    except:
+        print("file not found. Skipping")
+        continue
+    
+
+    curr_dat = None
+    for dat in data:
+        if dat["image_id"] == img_name:
+            curr_dat = dat
+
+    if curr_dat is None:
+        continue
+
+    try:
+        x0, y0, w, h = curr_dat["box"][0], curr_dat["box"][1], curr_dat["box"][2], curr_dat["box"][3]
+    except:
+        print(curr_dat["box"])
+        print("some error with the bounding box here. Skipping!")
+    
+
+    
+    update_dict(os.path.join(folder_name, img_name), asana, subject, x0, y0, w, h, curr_dat["keypoints"], frame_no)
+    total_data += 1
+
+pd.DataFrame(dataset_dict).to_csv("yadav_dataset_no_curation.csv", index = False)
+print("{} frames removed during curation. {} frames remain.".format(curated_removed, total_data))
+
+
+def process_dataset_to_get_largest_bbox(dataset_path, save_path):
+    dataset = pd.read_csv(dataset_path)
+    print(dataset.describe())
+    dataset["size"] = dataset["width"].values * dataset["height"].values
+    ids_max_bbox = dataset.groupby(['image_path'])["size"].idxmax()
+    dataset = dataset.iloc[ids_max_bbox, :]
+    dataset = dataset.drop(["size"], axis = 1)
+    print(dataset.describe())
+    dataset.to_csv(save_path, index=False)
+
+process_dataset_to_get_largest_bbox("yadav_dataset_no_curation.csv", "yadav_dataset_no_curation.csv")
---
+++ datasets/yadav/yadav_subject_wise.py
@@ -0,0 +1,64 @@
+from sklearn.model_selection import StratifiedKFold
+from utils import merge_dicts
+import os
+import pandas as pd
+
+from model import Classifier
+from utils import get_dataset, AccuracyMeter
+import numpy as np
+
+def subject_wise(no_trees = 500, max_depth = 8, dataset_path = "dataset.csv", save_model_path = "model.z", method = "random_forest", lr = 5, cfms_path = "confusion"):
+    X, Y = get_dataset(dataset_path)
+    print(X.shape)
+    os.makedirs(cfms_path, exist_ok=True)
+    predictions = np.asarray([-1 for i in range(len(Y))])
+    # print(Y[0])
+    dataset = pd.read_csv(dataset_path)
+    subjects = dataset["subject"]
+    subjects_list = subjects.unique()
+    classes = dataset["class"].unique()
+    accuracy_meter = AccuracyMeter()
+    rng = np.random.default_rng(1)
+    print(len(subjects), len(classes))
+    idx_list = rng.permutation(len(subjects_list)) 
+    # print(idx_list)
+    no_folds = 5
+    for i in range(no_folds):
+        if i == no_folds - 1:
+            current_test_idx = idx_list[3*i:]
+        else:
+            current_test_idx = idx_list[3*i:3*(i+1)]
+        current_subjs = [subjects_list[k] for k in current_test_idx]
+
+        train_index = [k for k in range(len(subjects)) if not subjects[k] in current_subjs]
+        test_index = [k for k in range(len(subjects)) if subjects[k] in current_subjs]
+        X_train = X[train_index]
+        Y_train = Y[train_index]
+        X_test = X[test_index]
+        Y_test = Y[test_index]
+
+        print("train: ", len(Y_train), "test: ", len(Y_test))
+        
+        classifier = Classifier(method, max_depth = max_depth, no_estimators = no_trees, lr = lr)
+
+
+        X_train, X_test = classifier.scale_vectors(X_train, X_test, "scaler.pkl")
+        classifier.train(X_train, Y_train)
+        confusion_plot_path = os.path.join(cfms_path, "fold_"+str(i))
+        accuracy = classifier.evaluate(X_test, Y_test, confusion_path = confusion_plot_path)
+
+        predictions[test_index] = classifier.model.predict(X_test)
+        print("The {} classifier has an accuracy of {}%".format(method, 100*accuracy))
+
+        is_best = accuracy_meter.update(accuracy)
+        if is_best:
+           classifier.save_model(save_model_path)
+
+        df_predictions = pd.DataFrame({"labels": Y, "predictions": predictions})
+        df_predictions.to_csv("yadav_predictions_subject_wise_no_curation.csv", index = False)
+   
+
+if __name__ == '__main__':
+    subject_wise(max_depth = None, dataset_path = "/Users/mustafa/Desktop/yoga/Yogasana-Classifier/preprocess/yadav_normalised_no_curation_dataset.csv", save_model_path = "yadav_best_model.z", cfms_path = "yadav_subject_wise")
+
+    
